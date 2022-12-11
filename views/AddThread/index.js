a5d7e9c7aeca7c58812fc33672d7935c42a360e1
bartek.jezierski <bartek.jezierski@glanos.de>
2022-12-11 19:52:54 +0100

    Enable adding docs to firebase

--- views/AddThread/index.js
+++ views/AddThread/index.js
@@ -13,44 +13,103 @@ import { firebase } from "../../firebase/config";
 import { ScrollView } from "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore, collection, addDoc, getDocs, setDoc, doc } from "firebase/firestore"; 
+import { Timestamp } from "@firebase/firestore";
+// const { InstantSearch, SearchBox, Hits, Highlight, Configure } = 'react-instantsearch-dom';
+// import { instantMeiliSearch } from '@meilisearch/instant-meilisearch'
+// const searchClient = instantMeiliSearch(
+//   "http://localhost:7700"
+// );
+
+// const AddThread = () => {
+//   const entityRef = firebase.firestore().collection("threads");
+//   const [entityText, setEntityText] = useState("");
+//   const [creator, setCreator] = useState("");
+//   const onAddButtonPress = () => {
+//     if (entityText && entityText.length > 0) {
+//       const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+//       const data = {
+//         childThreadID: "",
+//         commentIDArray: [],
+//         content: entityText,
+//         creatorName: creator,
+//         likes: 0,
+//         lineColor: "",
+//         lineName: "",
+//         lineNumber: "",
+//         motherThreadID: "",
+//         numberOfComments: 0,
+//         threadID: 0,
+//         locationID: "",
+//         timeStamp: timestamp,
+//         timeToLast: 3000,
+//       };
+//       entityRef
+//         .add(data)
+//         .then((_doc) => {
+//           setEntityText("");
+//           setCreator("");
+//           Keyboard.dismiss();
+//           navigation.goBack();
+//         })
+//         .catch((error) => {
+//           alert(error);
+//         });
+//     }
+//   };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1g_VTP4V4MBDeT8OJcB28ogjKedMNcAg",
+  authDomain: "mappit-c8d5d.firebaseapp.com",
+  projectId: "mappit-c8d5d",
+  storageBucket: "mappit-c8d5d.appspot.com",
+  messagingSenderId: "233256493371",
+  appId: "1:233256493371:web:00a95f440a246cde9ed71e",
+  measurementId: "G-WBQPW3XYQT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+// const auth = getAuth(app);
 
 const AddThread = () => {
   const navigation = useNavigation();
-  const entityRef = firebase.firestore().collection("threads");
+  const entityRef = collection(db, 'threads')
   const [entityText, setEntityText] = useState("");
   const [creator, setCreator] = useState("");
   const onAddButtonPress = () => {
+    console.log("Adding message: ", entityText);
+    console.log("By: ", creator);
     if (entityText && entityText.length > 0) {
-      const timestamp = firebase.firestore.FieldValue.serverTimestamp();
-      const data = {
-        childThreadID: "",
-        commentIDArray: [],
-        content: entityText,
-        creatorName: creator,
-        likes: 0,
-        lineColor: "",
-        lineName: "",
-        lineNumber: "",
-        motherThreadID: "",
-        numberOfComments: 0,
-        threadID: 0,
-        locationID: "",
-        timeStamp: timestamp,
-        timeToLast: 3000,
-      };
-      entityRef
-        .add(data)
-        .then((_doc) => {
-          setEntityText("");
-          setCreator("");
+        async function addNewDocument() {
+          const threadData = {
+            text: entityText,
+            date: Timestamp.fromDate(new Date()),
+            likes: 0,
+            creator: creator
+          };
+          const docRef = await addDoc(entityRef, threadData);
+          console.log("Document written with ID: ", docRef.id);
+          setEntityText('');
           Keyboard.dismiss();
           navigation.goBack();
-        })
-        .catch((error) => {
-          alert(error);
-        });
+        }
+        addNewDocument()
     }
-  };
+  }
+
+  // React.useEffect(() => {
+  //   navigation.setOptions({
+  //     headerRight: () => (
+  //       <TouchableOpacity style={styles.button2} onPress={console.log("no input")}>
+  //         <Text style={styles.buttonText}>Add</Text>
+  //       </TouchableOpacity>
+  //     ),
+  //   });
+  // }, []);
 
   React.useEffect(() => {
     navigation.setOptions({
