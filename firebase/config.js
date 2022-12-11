727486cd27b535053a00b921595a4f38188c5d21
cccblabla <47849409+cccblabla@users.noreply.github.com>
2022-12-11 16:15:12 +0100

    Add text input feature to AddThread
    Add text input to AddThread and integrate with firebase

---
+++ firebase/config.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+const firebaseConfig = {
+apiKey: '',
+authDomain: 'mappit-df955.firebaseapp.com',
+projectId: 'mappit-df955',
+storageBucket: 'mappit-df955.appspot.com',
+messagingSenderId: '681226213582',
+appId: 'insert yours: ',
+};
+if (!firebase.apps.length) {
+firebase.initializeApp(firebaseConfig);
+}
+export { firebase };
--- views/AddThread/index.js
+++ views/AddThread/index.js
@@ -1,14 +1,150 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, Keyboard, TextInput, TouchableOpacity, StyleSheet, Text, View, Button } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { firebase } from '../../firebase/config'
 
-const AddThread = () => {
+const AddThread = ({navigation}) => {
+
+  const entityRef = firebase.firestore().collection('threads')
+  const [entityText, setEntityText] = useState('')
+  const onAddButtonPress = () => {
+    if (entityText && entityText.length > 0) {
+        const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+        const data = {
+            childThreadID: "",
+            commentIDArray: [],
+            content: entityText,
+            creatorName: "",
+            likes: 0,
+            lineColor: "",
+            lineName: "",
+            lineNumber: "",
+            motherThreadID: "",
+            numberOfComments: 0,
+            threadID: 0,
+            locationID: "",
+            timeStamp: timestamp,
+            timeToLast: 3000,
+        };
+        entityRef
+            .add(data)
+            .then(_doc => {
+                setEntityText('')
+                Keyboard.dismiss()
+            })
+            .catch((error) => {
+                alert(error)
+            });
+    }
+  }
+
+
+  // React.useEffect(() => {
+  //   navigation.setOptions({
+  //     headerRight: () => (
+  //       <TouchableOpacity style={styles.button2} onPress={console.log("no input")}>
+  //         <Text style={styles.buttonText}>Add</Text>
+  //       </TouchableOpacity>
+  //     ),
+  //   });
+  // }, []);
+
+  React.useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity style={styles.button} onPress={onAddButtonPress}>
+          <Text style={styles.buttonText}>Add</Text>
+        </TouchableOpacity>
+      ),
+    });
+  }, [entityText]);
+
   return (
-    <View>
-      <Text>AddThread</Text>
+    <View style={styles.container}>
+        <View style={styles.formContainer}>
+            {/* <TouchableOpacity style={styles.button} onPress={onAddButtonPress}>
+                <Text style={styles.buttonText}>Add</Text>
+            </TouchableOpacity> */}
+            <TextInput
+                style={styles.input}
+                placeholder='What is happening now?'
+                placeholderTextColor="#aaaaaa"
+                onChangeText={(text) => setEntityText(text)}
+                value={entityText}
+                underlineColorAndroid="transparent"
+                autoCapitalize="none"
+                numberOfLines={20}
+                multiline={true}
+            />
+        </View>
     </View>
   )
 }
 
 export default AddThread
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  formContainer: {
+      flexDirection: 'column',
+      height: '100%',
+      width: '100%',
+      marginTop: 0,
+      marginBottom: 0,
+      flex: 1,
+      paddingTop: 0,
+      paddingBottom: 0,
+      paddingLeft: 0,
+      paddingRight: 0,
+      justifyContent: 'right',
+      alignItems: 'top'
+  },
+  input: {
+      width: '100%',
+      height: '100%',
+      borderRadius: 0,
+      overflow: 'hidden',
+      backgroundColor: 'white',
+      paddingLeft: 18,
+      paddingTop: 15,
+      flex: 1,
+      marginRight: 0,
+      fontSize:20
+  },
+  button: {
+      backgroundColor: "#24a0ed",
+      padding: 5,
+      borderRadius: 30,
+      paddingHorizontal: 20,
+      alignItems: "center",
+      justifyContent: 'center'
+  },
+  // button2: {
+  //   backgroundColor: "white",
+  //   padding: 5,
+  //   borderRadius: 30,
+  //   paddingHorizontal: 20,
+  //   alignItems: "center",
+  //   justifyContent: 'center'
+  // },
+  buttonText: {
+      color: 'white',
+      fontSize: 16
+  },
+  listContainer: {
+      marginTop: 20,
+      padding: 20,
+  },
+  entityContainer: {
+      marginTop: 16,
+      borderBottomColor: '#cccccc',
+      borderBottomWidth: 1,
+      paddingBottom: 16
+  },
+  entityText: {
+      fontSize: 20,
+      color: '#333333'
+  }
+})
