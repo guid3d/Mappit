1a8e269a3b4dbb3b43265e05b9c92dcb8c51e087
Guid3d <princegag1@gmail.com>
2023-02-02 15:02:29 +0100

    Add deviceID to likes attribute

--- components/ThreadLikeButton.js
+++ components/ThreadLikeButton.js
@@ -1,43 +1,69 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { firebaseConfig } from "../firebase/config";
 import { getFirestore, doc, updateDoc } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
-import firebase from "firebase/compat/app";
+import * as SecureStore from "expo-secure-store";
 
+import firebase from "firebase/compat/app";
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const increment = firebase.firestore.FieldValue.increment(1);
-const decrement = firebase.firestore.FieldValue.increment(-1);
+// const increment = firebase.firestore.FieldValue.increment(1);
+// const decrement = firebase.firestore.FieldValue.increment(-1);
 
 const ThreadLikeButton = ({ pressedLike, setPressedLike, item }) => {
+  const [deviceId, setDeviceID] = useState("");
   const [likes, setLikes] = useState(item.likes);
-  const threadRef = item.hasOwnProperty("commentID") ? (doc(db, "threads/" + item["threadID"] + "/comments", item["commentID"])) : (doc(db, "threads", item["threadID"]));
+  const threadRef = item.hasOwnProperty("commentID")
+    ? doc(db, "threads/" + item["threadID"] + "/comments", item["commentID"])
+    : doc(db, "threads", item["threadID"]);
+
+  useEffect(() => {
+    SecureStore.getItemAsync("secure_deviceid").then((fetchUUID) => {
+      if (fetchUUID) {
+        const parsedUUID = JSON.parse(fetchUUID);
+        setDeviceID(parsedUUID);
+        const alreadyLike = likes.find(
+          (likedByDeviceId) => (likedByDeviceId = parsedUUID)
+        );
+        if (alreadyLike) {
+          setPressedLike(true);
+        }
+      }
+    });
+  }, []);
   return (
     <View style={styles.iconAndText}>
       <TouchableOpacity
         onPress={() => {
-
-          // Creates a reference to the current thread 
+          // Creates a reference to the current thread
           // const threadRef = doc(db, "threads", item["threadID"]);
 
           setPressedLike((v) => !v);
-          setLikes((v) => (!pressedLike ? v + 1 : v - 1));
+          // setLikes((v) => (!pressedLike ? v + 1 : v - 1));
 
           // Now either increments or decrements the "Likes" of the specific thread
-          if(pressedLike) {
+          if (pressedLike) {
+            // remove deviceID from likes[]
+            const newLikes = likes.filter(
+              (likedByDeviceId) => likedByDeviceId != deviceId
+            );
+            console.log(newLikes);
+            setLikes(newLikes);
             updateDoc(threadRef, {
-              likes: decrement
+              likes: newLikes,
             });
           } else {
+            // add deviceID from likes[]
+            const newLikes = [...likes, deviceId];
+            console.log(newLikes);
+            setLikes(newLikes);
             updateDoc(threadRef, {
-              likes: increment
+              likes: newLikes,
             });
           }
-          
-
         }}
       >
         {pressedLike ? (
@@ -46,7 +72,7 @@ const ThreadLikeButton = ({ pressedLike, setPressedLike, item }) => {
           <Ionicons name="heart-outline" size={24} style={styles.icon} />
         )}
       </TouchableOpacity>
-      <Text style={styles.textAfterIcon}>{likes}</Text>
+      <Text style={styles.textAfterIcon}>{likes.length}</Text>
     </View>
   );
 };
